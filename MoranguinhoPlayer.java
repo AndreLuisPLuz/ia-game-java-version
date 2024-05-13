222dbfa84ed0de252f5a5d9c156aa221e9cd6aa5
AndreLuisPLuz <andreluispluz@gmail.com>
2024-05-13 13:02:26 -0300

    moranguinho funciona

--- MoranguinhoPlayer.java
+++ MoranguinhoPlayer.java
@@ -1,91 +1,69 @@
 import java.awt.Color;
-import java.util.ArrayList;
 
 public class MoranguinhoPlayer extends Player {
+    int i = 0;
+    Point enemy = null;
+    Point cornerToGo;
+    boolean isloading = false;
+
     public MoranguinhoPlayer(Point location) {
         super(location, Color.red, Color.green, "Moranguinho");
+
+        if (location.getX() <= 1000 && location.getY() <= 1000) {
+            cornerToGo = new Point(0, 0);
+        } else if (location.getX() <= 1000 && location.getY() >= 1000) {
+            cornerToGo = new Point(0, 2000);
+        } else if (location.getX() >= 1000 && location.getY() <= 1000) {
+            cornerToGo = new Point(1000, 0);
+        } else if (location.getX() >= 1000 && location.getY() >= 1000) {
+            cornerToGo = new Point(1000, 1000);
+        }
     }
 
-    boolean isSearchingEnemies = false;
-    boolean isAnalyzingSearch = false;
-    boolean isAttacking = false;
-
-    int cyclesPast = 0;
-    int fruitsEaten = 0;
-
-    Point enemy = null;
-    ArrayList<Point> closeEntities;
-
     @Override
-    protected void loop() {
-        if (cyclesPast == 0) {
-            AccurateSonar();
+    protected void loop()
+    {
+        enemy = 
+            getEnemiesInInfraRed().size() > 0 ?
+            getEnemiesInInfraRed().get(0) : null;
 
-            cyclesPast++;
-            return;
-        } else if (cyclesPast == 1) {
-            closeEntities = getEntitiesInAccurateSonar();
-            ResetSonar();
-
-            cyclesPast++;
-            return;
-        }
-
-        if (getEnergy() <= 50) {
+        if (getEnergy() < 30)
+        {
             StopMove();
-            ResetInfraRed();
-            ResetSonar();
-
-            cyclesPast++;
-            return;
+            isloading = true;
+            enemy = null;
         }
 
-        isSearchingEnemies = (closeEntities.size() > 0);
+        if (getEnergy() > 50)
+            isloading = false;
 
-        if (isSearchingEnemies && !isAnalyzingSearch) {
-            InfraRedSensorTo(closeEntities.get(0));
-            isAnalyzingSearch = true;
+        if (isloading)
+            return;
 
-            cyclesPast++;
+        if (enemy == null) {
+            InfraRedSensor(5f * i++);
             return;
         }
+        
+        InfraRedSensorTo(enemy);
 
-        if (isAnalyzingSearch && !isAttacking) {
-            enemy = 
-                getEnemiesInInfraRed().size() > 0 ?
-                getEnemiesInInfraRed().get(0) : null;
-    
-            if (enemy == null) {
-                isAnalyzingSearch = false;
-
-                cyclesPast++;
-                return;
-            }
-
-            isAttacking = true;
-
-            cyclesPast++;
-            return;
+        float dx = enemy.getX() - getLocation().getX();
+        float dy = enemy.getY() - getLocation().getY();
+        if (dx * dx + dy * dy <= 200f * 200f) {
+            ShootTo(enemy);
         }
-
-        if (isAttacking) {
-            if (getEnergy() >= 50) {
-                ShootTo(enemy);
-            }
-
-            isAttacking = false;
-
-            cyclesPast++;
-            return;
-        } else {
-            try {
-                MoveTo(closeEntities.get(0));
-            }
-            catch (Exception e) {
-                MoveTo(new Point(1080, 920));
-            }
+        else {
+            MoveTo(cornerToGo);
         }
-
-        cyclesPast++;
+        // float dx = enemy.getX() - getLocation().getX();
+        // float dy = enemy.getY() - getLocation().getY();
+        // if (dx * dx + dy * dy >= 300f * 300f)
+        //     MoveTo(enemy);
+        // else
+        // {
+        //     StopMove();
+        //     if (i++ % 5 == 0)
+        //         ShootTo(enemy);
+        // }
     }
 }
