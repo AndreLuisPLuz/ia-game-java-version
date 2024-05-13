be5d5862ec8e9d0281e7274b3769068772978901
akauzinha47 <kau.vmenezes@gmail.com>
2024-05-13 13:57:43 -0300

    O melhor atom de todos

--- Atom.java
+++ Atom.java
@@ -3,17 +3,21 @@ import java.awt.Color;
 public class Atom extends Player {
 
     public Atom(Point location) {
-        super(location, Color.black, Color.green, "Atom");
+        super(location, new Color(148, 0, 211, 100), Color.green, "Atom");
     }
 
     int searchindex = 0;
     int frame = 0;
     int points = 0;
     Point enemy = null;
+    Point enemy2 = null;
     boolean isloading = false;
     int i = 0;
-    int count_fires=0;
-    int stop_fire =0;
+    int j = 0;
+    int count_fires = 0;
+    int stop_fire = 0;
+    float dx;
+    float dy;
 
     @Override
     protected void loop() {
@@ -43,47 +47,63 @@ public class Atom extends Player {
         if (getEnergy() > 60)
             isloading = false;
 
+        if (getEnergy() > 90)
+            isloading = false;
+
         if (isloading)
             return;
 
-        InfraRedSensorTo(enemy);
-        float dx = enemy.getX() - getLocation().getX();
-        float dy = enemy.getY() - getLocation().getY();
-        if (dx * dx + dy * dy >= 500f * 500f)
-        {
-            AccurateSonar();
-            StopTurbo();
-            if (getEntitiesInAccurateSonar().isEmpty()) {
+        if (i++ % 5 == 0) {
+            InfraRedSensorTo(enemy);
+            dx = enemy.getX() - getLocation().getX();
+            dy = enemy.getY() - getLocation().getY();
+            if (dx * dx + dy * dy >= 700f * 700f) {
+                AccurateSonar();
+                StopTurbo();
+                if (getEntitiesInAccurateSonar().isEmpty()) {
+                    dx = enemy.getX() + 100f;
+                    dy = enemy.getY() + 100f;
+                    MoveTo(enemy);
+                } else {
+                    MoveTo(getEntitiesInAccurateSonar().get(0));
+                }
 
-            } else {
-                MoveTo(getEntitiesInAccurateSonar().get(0));
             }
 
         }
-        else if (dx * dx + dy * dy >= 300f * 300f) 
+
+        else if (dx * dx + dy * dy >= 500f * 500f)
+        {
+            dx = enemy.getX() + 400f;
+            dy = enemy.getY() + 400f;
             MoveTo(enemy);
+        }
         else {
-            StopMove();
-            if (count_fires >= 10) {
-                stop_fire++;    
-            }
+            // StopMove();
+            // if (count_fires >= 10) {
+            //     stop_fire++;
+            // }
 
-            if(stop_fire>=25){
-                count_fires =0;
-                stop_fire =0;
-            }
+            // if (stop_fire >= 25) {
+            //     count_fires = 0;
+            //     stop_fire = 0;
+            // }
 
-            if (count_fires <=10) {
-                if (i++ % 1 == 0){
+            // if (count_fires <= 5) {
+            //     if (i++ % 1 == 0) {
+            //         ShootTo(enemy);
+            //         count_fires++;
+
+            //     }
+            // }
+            if (j++ % 5 == 0) {
                     ShootTo(enemy);
-                    count_fires++;
+                    enemy2 = new Point((enemy.getX()), (enemy.getY() + 300f));
+                    MoveTo(enemy2);
                     
+
                 }
-            }
 
-            
-
-          
         }
 
     }
